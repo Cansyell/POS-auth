5ebc5a387bea4865cd1fd24b48718ab1bc41c1fe
Cansyell <haiguyswehhh@gmail.com>
2025-06-04 23:21:56 +0700

    feat: fix server.js and route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,11 @@ const authController = require('../controllers/authController');
 // const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', authController.register);
-router.post('/login', authController.login);
+// router.post('/login', authController.login);
+router.post('/login', (req, res, next) => {
+  console.log('Login payload:', req.body);
+  authController.login(req, res, next);
+});
 // Add the logout route - requiring authentication
 router.post('/logout', authController.logout);
 router.get('/getUser', authController.getUser);
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 
 // Middlewares
 app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
